5f57f82c1d2647a2ef0ee874e50ca7fd1196f26c
Sergio Gennari <sergio.gennari@softeco.it>
2020-06-25 18:18:57 +0200

    the micro webc

---
+++ micro frontend/app/src/appLayout.js
@@ -0,0 +1,14 @@
+class AppLayout extends HTMLElement {
+  constructor() {
+    super();
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = `.appLayout { background-color: #FFF; display: flex; flex-direction: column; position: absolute; top: 0px; left: 0px; height: 100vh; width: 100vw;}`;
+    document.getElementsByTagName('head')[0].appendChild(style);
+    this.className = 'appLayout';
+  }
+  connectedCallback() {
+
+  }
+}
+window.customElements.define("app-layout", AppLayout);
