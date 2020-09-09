af25150b1bbb2469117c48cf5cdcebb5a2920b01
Sergio Gennari <sergio.gennari@softeco.it>
2020-09-09 09:57:27 +0200

    fix load

--- micro frontend/app/src/utils.js
+++ micro frontend/app/src/utils.js
@@ -1,18 +1,17 @@
-
 export function registerElement(name, src) {
-    return new Promise((resolve, reject) => {
-        // dynamically insert script (if doesn't already exist)
-        if (!document.getElementById(name)) {
-            const script = document.createElement('script');
-            script.src = src;
-            script.id = name;
-            script.onload = () => {
-                resolve();
-            }
-            script.onerror = e => {
-                reject(e);
-            }
-            document.body.appendChild(script);
-        }
-    });
+  return new Promise((resolve, reject) => {
+    // dynamically insert script (if doesn't already exist)
+    if (!document.getElementById(name)) {
+      const script = document.createElement("script");
+      script.src = src;
+      script.id = name;
+      script.onload = () => {
+        resolve();
+      };
+      script.onerror = (e) => {
+        reject(e);
+      };
+      document.body.appendChild(script);
+    } else resolve();
+  });
 }
