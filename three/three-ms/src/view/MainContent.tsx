b7ac2b745e2b6651cefaceb05b98db641661bdd3
oufresh <sergio.gennari@softeco.it>
2021-05-08 17:23:37 +0200

    layout

--- three/three-ms/src/view/MainContent.tsx
+++ three/three-ms/src/view/MainContent.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { AppContent } from "./AppContent";
 import style from "./MainContent.module.css";
 import { Toolbar } from "./Toolbar";
+import { NavMenu} from "./NavMenu";
 
 export const MainContent = () => {
     return <main className={style.MainContent}>
+        <NavMenu />
         <AppContent />
         <Toolbar />
     </main>;
---
+++ three/three-ms/src/view/NavMenu.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./NavMenu.module.css";
+
+export const NavMenu = () => {
+  return <aside className={styles.NavMenu} />;
+};
