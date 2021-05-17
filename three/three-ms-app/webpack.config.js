1e7156cdd08294b9620603acf84c2d58dfabe46b
oufresh <sergio.gennari@softeco.it>
2021-05-17 10:14:05 +0200

    fix

--- three/three-ms-app/webpack.config.js
+++ three/three-ms-app/webpack.config.js
@@ -6,9 +6,15 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 /**
+ * non può funzionare così perchè prima si carica lo script dall'app principale e il web component non è nel dom, in questo momento il loade prova inserire il
+ * css. Quando lo renderizza nel dom il loader ormai ha già girato.
+ * Posso allora 
+ * 1. usare un loader raw e inserire il css creando il nodo style nel web component come fatto qui
+ * 2. prefixare il css e le classi per non andare in conflitto con quello dell'app principale e caricarlo con style loader nle dom principale (prefix css-loader)
+ * 3. usare un tag style con link e caricare il css separatamente
  * 
  * document.querySelector("basket-list").shadowRoot.firstElementChild
- */
+ *document.querySelector("three-ms-app").shadowRoot.querySelector("style")
 /*
 const customStyleLoader = {
   loader: 'style-loader',
--- three/three-ms/src/view/AppContent.tsx
+++ three/three-ms/src/view/AppContent.tsx
@@ -1,51 +1,10 @@
 import React, { /*useState, useEffect, useRef */ } from "react";
 import style from "./AppContent.module.css";
 import { useLoad, LoadEnum } from "../hooks/load";
-/*
-function creaCodiceHtml() {
-  return { __html: "<three-ms-app />" };
-}*/
+
 
 const AppContent = () => {
-  //const [loaded, setLoaded] = useState<LoadEnum |null>(null);
   const load = useLoad();
-  //const scriptTag = useRef<HTMLScriptElement | null>(null); //fix current null readonly
-  //useEffect(() => {
-    /*function loaded() {
-      setLoaded(LoadEnum.SUCCESS);
-      console.log("script loaded");
-    }
-    async function load() {
-      try {
-        setLoaded(LoadEnum.LOADING);
-        const manifest = await (
-          await fetch("/three-ms-app/manifest.json")
-        ).json();
-        console.log(manifest);
-
-        const s = document.getElementById(manifest["name"]);
-        if (s != null) throw new Error("script already loaded");
-
-        //load component
-        scriptTag.current = document.createElement("script");
-        scriptTag.current.id = manifest["name"];
-        scriptTag.current.src = `/${manifest["name"]}/${manifest["main.js"]}`;
-        scriptTag.current.async = true;
-        scriptTag.current.addEventListener("load", loaded);
-        document.body.appendChild(scriptTag.current);
-      } catch (e) {
-        console.error(e);
-        setLoaded(LoadEnum.ERROR);
-      }
-    }
-    load();
-
-    return function dispose() {
-      scriptTag.current?.removeEventListener("load", loaded);
-      scriptTag.current?.parentNode?.removeChild(scriptTag.current);
-      console.log("dispose");
-    };*/
-  //}, []);
   if (load === LoadEnum.SUCCESS) {
     return (
       <div className={style.AppContent}>
@@ -57,6 +16,4 @@ const AppContent = () => {
   } else return <div className={style.AppContent}></div>;
 };
 
-//
-//divdangerouslySetInnerHTML={creaCodiceHtml()
 export default React.memo(AppContent);
